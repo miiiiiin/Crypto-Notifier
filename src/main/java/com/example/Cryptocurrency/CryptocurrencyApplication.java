41cfd3da15ef9921ae003b487aa582dffbcf4992
songkyung <min.songkyung@gmail.com>
2025-01-15 16:57:27 +0900

    fix: application yml 파일 분리

--- src/main/java/com/example/Cryptocurrency/CryptocurrencyApplication.java
+++ src/main/java/com/example/Cryptocurrency/CryptocurrencyApplication.java
@@ -7,13 +7,15 @@ import com.example.Cryptocurrency.http.UpbitHttpClient;
 import com.example.Cryptocurrency.service.UpbitSlackService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CryptocurrencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptocurrencyApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CryptocurrencyApplication.class, args);
+		SpringApplication.exit(run);
 	}
 
 }
